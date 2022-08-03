459add827ed34f553ff5e2ea3b83576b4ae12bf1
claire bernard <claire.bernard@bluesoft-group.com>
2022-08-03 16:50:52 +0200

    update css 03/08/22

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { NavbarBrand } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,7 +8,15 @@ function CollapsibleExample() {
     // <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/home">Pokemon App</Navbar.Brand>
+        <NavbarBrand href="/home">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg"
+            alt=""
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+          />
+        </NavbarBrand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Banner from "../components/Banner";
 import NavBar from "../components/NavBar";
 import CardPokemon from "../components/CardPokemon";
 import getPokemonList from "../services/pokemonListCard";
@@ -7,9 +6,9 @@ import { useState, useEffect } from "react";
 import usePokemons from "../services/pokemonListCard";
 import PokemonList from "../components/PokemonList";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 function Home(props) {
   const [data, setData] = useState();
@@ -20,15 +19,8 @@ function Home(props) {
   return (
     <div>
       <NavBar />
-      {/* <Container>
-        <Row>
-          <Col sm>
-            <PokemonList pokemonList={pokemonsList} />
-          </Col>
-        </Row>
-      </Container> */}
       <div>
-        <input type="text" placeholder="Rechercher un pokemon.."></input>
+        {/* <input type="text" placeholder="Rechercher un pokemon.."></input> */}
       </div>
       <PokemonList pokemonList={pokemonsList} />
     </div>
